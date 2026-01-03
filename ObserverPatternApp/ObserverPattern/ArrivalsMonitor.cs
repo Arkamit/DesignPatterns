481383c28950e1dc1f054ded20ccc35387637c5a
Arkamit <arkamitdutta@gmail.com>
2026-01-03 11:42:39 +0530

    Implementing the Observer Pattern in .NetCore C#

---
+++ ObserverPatternApp/ObserverPattern/ArrivalsMonitor.cs
@@ -0,0 +1,70 @@
+﻿namespace ObserverPattern;
+
+public class ArrivalsMonitor : IObserver<BaggageInfo>
+{
+    private readonly string _name;
+    private readonly List<string> _flights = new();
+    private readonly string format = "{0, -20} {1, 5}  {2, 3}";
+    private IDisposable? _cancellation;
+
+    public ArrivalsMonitor(string name)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        _name = name;
+    }
+
+    public virtual void Subscribe(BaggageHandler provider) =>
+        _cancellation = provider.Subscribe(this);
+
+    public virtual void Unsubscribe()
+    {
+        _cancellation?.Dispose();
+        _flights.Clear();
+    }
+
+    public virtual void OnCompleted() => _flights.Clear();
+
+    public void OnError(Exception error)
+    {
+        
+    }
+
+    public virtual void OnNext(BaggageInfo info)
+    {
+        bool updated = false;
+
+        if (info.Carousel is 0)
+        {
+            string flightNumber = string.Format("{0, 5}", info.FlightNumber);
+            for (int index = _flights.Count - 1; index >= 0; index--)
+            {
+                string flightInfo = _flights[index];
+                if (flightInfo.Substring(21, 5).Equals(flightNumber))
+                {
+                    updated = true;
+                    _flights.RemoveAt(index);
+                }
+            }
+        }
+        else
+        {
+            string flightInfo = string.Format(format, info.From, info.FlightNumber, info.Carousel);
+            if (_flights.Contains(flightInfo) is false)
+            {
+                updated = true;
+                _flights.Add(flightInfo);
+            }
+        }
+
+        if (updated)
+        {
+            _flights.Sort();
+            Console.WriteLine($"Arrivals information from {_name}");
+            foreach (string flight in _flights)
+            {
+                Console.WriteLine(flight);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ObserverPatternApp/ObserverPattern/BaggageHandler.cs
@@ -0,0 +1,63 @@
+﻿using ObserverPattern;
+
+public readonly record struct BaggageInfo
+    (int FlightNumber,
+    string From,
+    int Carousel);
+
+public sealed class BaggageHandler : IObservable<BaggageInfo>
+{
+    private readonly HashSet<IObserver<BaggageInfo>> _observers = new();
+    private readonly HashSet<BaggageInfo> _flights = new();
+
+    public IDisposable Subscribe(IObserver<BaggageInfo> observer)
+    {
+        // Check if observer is already registered. If not, add it
+        if (_observers.Add(observer))
+        {
+            // Provide observer with existing data
+            foreach (var item in _flights)
+            {
+                observer.OnNext(item);
+            }
+        }
+
+        return new Unsubscriber<BaggageInfo>(_observers, observer);
+    }
+
+    public void BaggageStatus(int flightNumber) =>
+        BaggageStatus(flightNumber, string.Empty, 0);
+
+    public void BaggageStatus(int flightNumber, string from, int carousel)
+    {
+        var info = new BaggageInfo(flightNumber, from, carousel);
+
+        if (carousel > 0 && _flights.Add(info))
+        {
+            foreach (IObserver<BaggageInfo> observer in _observers)
+            {
+                observer.OnNext(info);
+            }
+        }
+        else if (carousel is 0)
+        {
+            if (_flights.RemoveWhere(
+                flight => flight.FlightNumber == info.FlightNumber) > 0)
+            {
+                foreach (IObserver<BaggageInfo> observer in _observers)
+                {
+                    observer.OnNext(info);
+                }
+            }
+        }
+    }
+
+    public void LastBaggageClaimed()
+    {
+        foreach (IObserver<BaggageInfo> observer in _observers)
+        {
+            observer.OnCompleted();
+        }
+        _observers.Clear();
+    }
+}
